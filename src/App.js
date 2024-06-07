013da0b1d9a2fe9e4054ab7ed20d9fa8668cbac6
Rafal Chudy <rafalchu5@gmail.com>
2024-06-07 20:09:47 +0200

    Added new logo icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import { logoIcon } from "../public/logo192.png";
 
 function App() {
     <div className="App">
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logoIcon} className="logo-icon" alt="logo" />
             <p>Tekst na stronie</p>
             Learn React
         </header>
