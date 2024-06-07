61cf8aadd973632fb9088442b32056b832a4d1fb
Rafal Chudy <rafalchu5@gmail.com>
2024-06-07 19:42:57 +0200

    Changed text in paragraph

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      Learn React
-    </header>
-  </div >
+    <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Tekst na stronie</p>
+            Learn React
+        </header>
+    </div >
 };
 
 export default App;
